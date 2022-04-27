e0fdde5df79a0a8336d62822b54851411e9f39da
Goupil117 <84926681+Goupil117@users.noreply.github.com>
2022-04-27 21:40:43 +0200

    Add files via upload

---
+++ XamarinApp/Models/RendezVous.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+
+namespace XamarinApp.Models
+{
+    public class RendezVous
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public TimeSpan Heure { get; set; }
+
+    }
+}
